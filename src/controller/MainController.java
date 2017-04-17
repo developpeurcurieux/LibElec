9e656371ff4fb6451c3d87aea1d20928ec8b7e93
developpeurcurieux <developpeurcurieux@gmail.com>
2017-04-17 07:25:10 +0200

    add two frames with controllers

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -2,6 +2,8 @@
 package controller;
 import model.*;
 import view.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainController {
   private WelcomeModel welcomeModel;
@@ -10,9 +12,31 @@ public class MainController {
   public MainController(WelcomeModel welcomeModel, WelcomeFrame welcomeFrame) {
       this.welcomeModel = welcomeModel;
       this.welcomeFrame = welcomeFrame;
+      welcomeFrame.setController(this);
       welcomeFrame.setVisible(true);
-                    
   }
+  
+  
+  
+  
+  
+  
+  public void quit() {
+      welcomeFrame.setEnabled(false);
+      welcomeFrame.setVisible(false);
+      System.exit(0);
+  }
+
+  public void changeFrames() {
+      welcomeFrame.setEnabled(false);
+      welcomeFrame.setVisible(false);
+      //
+      UpdateFrame updateFrame = new UpdateFrame();
+      UpdateController updateController = new UpdateController(updateFrame);
+      
+
+  }
+      
   
   
   
--- src/view/WelcomeFrame.java
+++ src/view/WelcomeFrame.java
@@ -1,115 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package view;
 
-/**
- *
- * @author testo
- */
-public class WelcomeFrame extends javax.swing.JFrame {
-     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
-    /**
-     * Creates new form NewJFrame
-     */
+import controller.*;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
+
+
+
+
+public class WelcomeFrame extends JFrame {
+    private JButton btnValider;
+    private JButton btnQuitter;
+    private JLabel lblBienvenue;
+    private JPanel jPanel1;
+    private JPasswordField jPswField;
+    private JTextField jTextField1;
+    private MainController mainController;
+    
+    
+    
     public WelcomeFrame() {
         initGuiLAF();
         initComponents();
+       
     }
-
-   
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+     
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jPanel1 = new JPanel();
+        lblBienvenue = new JLabel();
+        jTextField1 = new JTextField();
+        jPswField = new JPasswordField();
+        btnValider = new JButton();
+        btnQuitter = new JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
         jPanel1.setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Droid Sans Mono", 1, 18)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Bienvenue LibElec");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(150, 45, 250, 40);
+        lblBienvenue.setFont(new Font("Droid Sans Mono", 1, 18)); // NOI18N
+        lblBienvenue.setHorizontalAlignment(SwingConstants.CENTER);
+        lblBienvenue.setText("Bienvenue LibElec");
+        jPanel1.add(lblBienvenue);
+        lblBienvenue.setBounds(150, 45, 250, 40);
 
-        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField1.setHorizontalAlignment(JTextField.CENTER);
         jTextField1.setText("Login");
         jPanel1.add(jTextField1);
         jTextField1.setBounds(160, 220, 220, 40);
 
-        jPasswordField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jPasswordField1.setText("*******************");
-        jPanel1.add(jPasswordField1);
-        jPasswordField1.setBounds(159, 271, 220, 40);
+        jPswField.setHorizontalAlignment(JTextField.CENTER);
+        jPswField.setText("*******************");
+        jPanel1.add(jPswField);
+        jPswField.setBounds(159, 271, 220, 40);
 
-        jButton1.setText("valider");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton1);
-        jButton1.setBounds(310, 330, 78, 31);
+        btnValider.setText("valider");
+        
+        jPanel1.add(btnValider);
+        btnValider.setBounds(310, 330, 78, 31);
+        
+        btnValider.addActionListener(e -> mainController.changeFrames());
 
-        jButton2.setText("quitter");
-        jPanel1.add(jButton2);
-        jButton2.setBounds(30, 430, 79, 31);
+        btnQuitter.setText("quitter");
+        
+        jPanel1.add(btnQuitter);
+        btnQuitter.setBounds(30, 430, 79, 31);
+        
+        btnQuitter.addActionListener(e -> mainController.quit());
 
         getContentPane().add(jPanel1);
         jPanel1.setBounds(0, 0, 500, 500);
 
         setBounds(0, 0, 510, 530);
-    }// </editor-fold>                        
+    }                        
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
-    }                                        
-    
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent event) {
-        
-        jButton2.addActionListener(e -> quitProgram());
-    }
-    /**
-     * @param args the command line arguments
-     */
+   
     public void initGuiLAF() {
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(WelcomeFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
+        
     }
     
-    public void quitProgram() {
-        this.setEnabled(false);
-        System.exit(0);
+    public void setController(MainController mainController) {
+        this.mainController = mainController;
     }
+    
+    
    
-    // End of variables declaration                   
+   
 }

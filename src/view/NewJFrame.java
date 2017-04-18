d242bb1ae10d0a3a8ac424187cae3189b25b9968
developpeurcurieux <developpeurcurieux@gmail.com>
2017-04-18 20:52:31 +0200

    update

--- src/view/NewJFrame.java
+++ src/view/NewJFrame.java
@@ -37,6 +37,14 @@ public class NewJFrame extends javax.swing.JFrame {
         jTree1 = new javax.swing.JTree();
         jScrollPane3 = new javax.swing.JScrollPane();
         jTable3 = new javax.swing.JTable();
+        jPanel2 = new javax.swing.JPanel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -67,17 +75,17 @@ public class NewJFrame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(850, 500));
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(100, 700));
+        jPanel1.setPreferredSize(new java.awt.Dimension(150, 700));
         jPanel1.setLayout(null);
 
         jButton1.setText("chercher");
         jPanel1.add(jButton1);
-        jButton1.setBounds(0, 340, 92, 40);
+        jButton1.setBounds(10, 230, 140, 30);
 
         jScrollPane4.setViewportView(jTree1);
 
         jPanel1.add(jScrollPane4);
-        jScrollPane4.setBounds(2, 3, 100, 330);
+        jScrollPane4.setBounds(2, 3, 150, 220);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.LINE_START);
 
@@ -95,6 +103,42 @@ public class NewJFrame extends javax.swing.JFrame {
         jScrollPane3.setViewportView(jTable3);
 
         getContentPane().add(jScrollPane3, java.awt.BorderLayout.CENTER);
+
+        jPanel2.setPreferredSize(new java.awt.Dimension(500, 100));
+        jPanel2.setLayout(null);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jPanel2.add(jComboBox1);
+        jComboBox1.setBounds(200, 40, 130, 25);
+
+        jTextField1.setText("Nom");
+        jPanel2.add(jTextField1);
+        jTextField1.setBounds(370, 40, 130, 23);
+
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Element a modifier");
+        jPanel2.add(jLabel1);
+        jLabel1.setBounds(200, 10, 140, 20);
+
+        jButton2.setText("valider");
+        jPanel2.add(jButton2);
+        jButton2.setBounds(550, 40, 110, 30);
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("User");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(10, 20, 60, 30);
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.PAGE_START);
+
+        jPanel3.setPreferredSize(new java.awt.Dimension(500, 30));
+        jPanel3.setLayout(null);
+
+        jButton3.setText("Quitter");
+        jPanel3.add(jButton3);
+        jButton3.setBounds(10, 0, 140, 30);
+
+        getContentPane().add(jPanel3, java.awt.BorderLayout.PAGE_END);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -136,7 +180,14 @@ public class NewJFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
@@ -144,6 +195,7 @@ public class NewJFrame extends javax.swing.JFrame {
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
     private javax.swing.JTable jTable3;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTree jTree1;
     // End of variables declaration//GEN-END:variables
 }
